73beed8ea05f5c9f443bedc6d04375ecec85d036
nerdpapi <kunalkmr71@gmail.com>
2026-08-19 19:26:38 +0530

    Add LeetCode 2235 Add Two Integers

---
+++ LeetCode/Math/2235-Add-Two-Integers.py
@@ -0,0 +1,15 @@
+"""
+Problem: 2235. Add Two Integers
+Platform: LeetCode
+Difficulty: Easy
+
+Approach:
+Return the sum of the two integers.
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+"""
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
