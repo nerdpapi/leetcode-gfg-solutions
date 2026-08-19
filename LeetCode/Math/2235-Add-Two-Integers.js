4d8b385ea0322a60f983c70ba344473dc6f09739
nerdpapi <kunalkmr71@gmail.com>
2026-08-19 19:26:57 +0530

    Add LeetCode 2235 Add Two Integers

---
+++ LeetCode/Math/2235-Add-Two-Integers.js
@@ -0,0 +1,20 @@
+/**
+ * Problem: 2235. Add Two Integers
+ * Platform: LeetCode
+ * Difficulty: Easy
+ *
+ * Approach:
+ * Return the sum of the two integers.
+ *
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function(num1, num2) {
+    return num1 + num2;
+};
